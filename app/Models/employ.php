353fd2e6304d606d34f8eb748cb73f255e5231ae
Wilmsy <masangcaywilma@gmail.com>
2024-05-29 21:41:19 +0800

    hello

---
+++ app/Models/employ.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class employ extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $table = 'employee';
+    protected $primarykey='id';
+    protected $fillable =  ['fname','mname','lname','address','country','state'	
+    ,'city','zip','age','bdate','dhired','dept',  'div'	
+];
+}
